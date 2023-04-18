bef3ba6dd34227ae0689ed2bf22c6b4a541ab1dd
Guilherme Cosme <cosme@huntress.huntress>
2023-04-18 16:18:01 -0300

    adding desafio 2.9

---
+++ 2.9 - Desafio: variáveis e operadores aritiméticos/TicketMedio.java
@@ -0,0 +1,11 @@
+public class TicketMedio {
+    
+    public static void main(String[] args) {
+
+        int ticketA = 10;
+        int ticketB = 30;
+        int ticketC = 100;
+        int ticketMedio = (ticketA + ticketB + ticketC) / 3;
+        System.out.println(" O ticket médio corresponde a : " +  ticketMedio);
+    }
+}
